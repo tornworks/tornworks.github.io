4f2e2b2e3c69cb59654456fd9e495573a0a2f7a7
Steve T <tornworks@gmail.com>
2017-11-13 12:32:07 +0300

    nav alive + topbutton

--- js/main.js
+++ js/main.js
@@ -1,60 +1,32 @@
-<script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-  <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
-  <script type="text/javascript">
-    $(document).on('ready', function() {
-      $(".vertical-center-4").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-        slidesToShow: 4,
-        slidesToScroll: 2
-      });
-      $(".vertical-center-3").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-      $(".vertical-center-2").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-        slidesToShow: 2,
-        slidesToScroll: 2
-      });
-      $(".vertical-center").slick({
-        dots: true,
-        vertical: true,
-        centerMode: true,
-      });
-      $(".vertical").slick({
-        dots: true,
-        vertical: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-      $(".regular").slick({
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3
-      });
-      $(".center").slick({
-        dots: true,
-        infinite: true,
-        centerMode: true,
-        slidesToShow: 5,
-        slidesToScroll: 3
-      });
-      $(".variable").slick({
-        dots: true,
-        infinite: true,
-        variableWidth: true
-      });
-      $(".lazy").slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true
-      });
-    });
-</script>
+$(function(){
+
+$('.about-me_btn ,.arrow_down').on('click', function(e){
+  $('html,body').stop().animate({ scrollTop: $('#about-me_anc').offset().top }, 1000);
+  e.preventDefault();
+});
+
+});
+$(function(){
+
+$('.skills_btn').on('click', function(e){
+  $('html,body').stop().animate({ scrollTop: $('#skills_anc').offset().top }, 1000);
+  e.preventDefault();
+});
+
+});
+$(function(){
+
+$('.works_btn').on('click', function(e){
+  $('html,body').stop().animate({ scrollTop: $('#works_anc').offset().top }, 1000);
+  e.preventDefault();
+});
+
+});
+$(function(){
+
+$('.topbnt').on('click', function(e){
+  $('html,body').stop().animate({ scrollTop: $('.header__bg').offset().top }, 1000);
+  e.preventDefault();
+});
+
+});
