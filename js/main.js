4ab1926cc78533a6b08df10ccc9dfdfe8757d48c
Steve T <tornworks@gmail.com>
2017-11-15 15:17:54 +0300

    menu-mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+$(document).ready(function() {
+
+	var pull 		= $('#pull');
+	menu 		= $('.menu-mobile ul');
+	menuHeight	= menu.height();
+
+	$(pull).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 320 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+
+	
+	$(".service-item").fancybox({
+		padding: 0
+	});
+
+	$(".fancybox").fancybox({
+		padding: 0
+	});
+
+});
 $(document).ready(function() { 
      //кнопки
 	$('.about-me_btn ,.arrow_down').on('click', function(e){
